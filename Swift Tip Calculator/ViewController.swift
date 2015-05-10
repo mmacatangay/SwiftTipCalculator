74188a0098ebec40c1fb471225eca3564b91aae4
Michael Macatangay <mmacatangay@delivery.com>
2015-05-10 14:19:58 -0400

    initial commit

--- Swift Tip Calculator/ViewController.swift
+++ Swift Tip Calculator/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtMealCost: UITextField!
+    @IBOutlet weak var txtTip: UITextField!
+    
+    @IBOutlet weak var lblTipCalculated: UILabel!
+    
+    var mealCost = ""
+    var tipPercentage = ""
+    var answer : Float = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +28,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnCalculate(sender: UIButton) {
+        calculateTip()
+    }
+    
 
-
+    @IBAction func btnClear(sender: UIButton) {
+        txtMealCost.text = ""
+        txtTip.text = ""
+        
+        lblTipCalculated.text = "Tip Total"
+    }
+  
+    //Calculate tip
+    func calculateTip() -> Bool {
+        mealCost = txtMealCost.text
+        tipPercentage = txtTip.text
+        
+        var fMealCost = (mealCost as NSString).floatValue
+        var fTipPercentage = (tipPercentage as NSString).floatValue
+        
+        answer = fMealCost * (fTipPercentage * 0.01)
+        
+        var strAnswerFormat : NSString = NSString(format: "%0.2f", answer)
+        
+        lblTipCalculated.text = "Tip: $\(strAnswerFormat)"
+        
+        return true
+    }
 }
 
